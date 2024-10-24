7f29e6c31a583cf55cabd58e805ad2ecf1735209
Alex Kusek <alexander.kusek@marquette.edu>
2024-10-24 17:17:55 -0500

    n-step and heuristic agent implementation

---
+++ nstep-heuristic.py
@@ -0,0 +1,143 @@
+# Authors: Alex Kusek, Athziri Garcia, Shayla Salamon Ruiz, Ayushi Maurya
+# Date: 24 October 2024
+# Description: Implement a bot that uses n-step look ahead and your own board evaluation heuristic
+#              Code from Alexis Cook's One-Step Lookahead and N-Step Lookahead tutorials
+
+from kaggle_environments import make, evaluate
+import random
+import numpy as np
+
+# Helper function for get_heuristic: checks if window satisfies heuristic conditions
+def check_window(window, num_discs, piece, config):
+    return (window.count(piece) == num_discs and window.count(0) == config.inarow-num_discs)
+    
+# Helper function for get_heuristic: counts number of windows satisfying specified heuristic conditions
+def count_windows(grid, num_discs, piece, config):
+    num_windows = 0
+    # horizontal
+    for row in range(config.rows):
+        for col in range(config.columns-(config.inarow-1)):
+            window = list(grid[row, col:col+config.inarow])
+            if check_window(window, num_discs, piece, config):
+                num_windows += 1
+    # vertical
+    for row in range(config.rows-(config.inarow-1)):
+        for col in range(config.columns):
+            window = list(grid[row:row+config.inarow, col])
+            if check_window(window, num_discs, piece, config):
+                num_windows += 1
+    # positive diagonal
+    for row in range(config.rows-(config.inarow-1)):
+        for col in range(config.columns-(config.inarow-1)):
+            window = list(grid[range(row, row+config.inarow), range(col, col+config.inarow)])
+            if check_window(window, num_discs, piece, config):
+                num_windows += 1
+    # negative diagonal
+    for row in range(config.inarow-1, config.rows):
+        for col in range(config.columns-(config.inarow-1)):
+            window = list(grid[range(row, row-config.inarow, -1), range(col, col+config.inarow)])
+            if check_window(window, num_discs, piece, config):
+                num_windows += 1
+    return num_windows
+
+# Helper function for minimax_alpha_beta: calculates value of heuristic for grid
+def get_heuristic(grid, mark, config):
+    num_threes = count_windows(grid, 3, mark, config)
+    num_fours = count_windows(grid, 4, mark, config)
+    num_threes_opp = count_windows(grid, 3, mark%2+1, config)
+    num_fours_opp = count_windows(grid, 4, mark%2+1, config)
+    score = num_threes - 1e2*num_threes_opp - 1e4*num_fours_opp + 1e6*num_fours
+    return score
+    
+# Uses minimax_alpha_beta to calculate value of dropping piece in selected column
+def score_move(grid, col, mark, config, nsteps):
+    next_grid = drop_piece(grid, col, mark, config)
+    score = minimax_alpha_beta(next_grid, nsteps-1, False, mark, config)
+    return score
+
+# Helper function for minimax_alpha_beta: checks if agent or opponent has four in a row in the window
+def is_terminal_window(window, config):
+    return window.count(1) == config.inarow or window.count(2) == config.inarow
+
+# Helper function for minimax_alpha_beta: checks if game has ended
+def is_terminal_node(grid, config):
+    # Check for draw 
+    if list(grid[0, :]).count(0) == 0:
+        return True
+    # Check for win: horizontal, vertical, or diagonal
+    # horizontal 
+    for row in range(config.rows):
+        for col in range(config.columns-(config.inarow-1)):
+            window = list(grid[row, col:col+config.inarow])
+            if is_terminal_window(window, config):
+                return True
+    # vertical
+    for row in range(config.rows-(config.inarow-1)):
+        for col in range(config.columns):
+            window = list(grid[row:row+config.inarow, col])
+            if is_terminal_window(window, config):
+                return True
+    # positive diagonal
+    for row in range(config.rows-(config.inarow-1)):
+        for col in range(config.columns-(config.inarow-1)):
+            window = list(grid[range(row, row+config.inarow), range(col, col+config.inarow)])
+            if is_terminal_window(window, config):
+                return True
+    # negative diagonal
+    for row in range(config.inarow-1, config.rows):
+        for col in range(config.columns-(config.inarow-1)):
+            window = list(grid[range(row, row-config.inarow, -1), range(col, col+config.inarow)])
+            if is_terminal_window(window, config):
+                return True
+    return False
+
+# Minimax_alpha_beta Implementation
+def minimax_alpha_beta(node, depth, alpha, beta, maximizingPlayer, mark, config):
+    is_terminal = is_terminal_node(node, config)
+    valid_moves = [c for c in range(config.columns) if node[0][c] == 0]
+    
+    if depth == 0 or is_terminal:
+        return get_heuristic(node, mark, config)
+    
+    if maximizingPlayer:
+        value = -np.Inf
+        for col in valid_moves:
+            child = drop_piece(node, col, mark, config)
+            value = max(value, minimax_alpha_beta(child, depth-1, alpha, beta, False, mark, config))
+            alpha = max(alpha, value)
+            if alpha >= beta:
+                break  # Beta cut-off
+        return value
+    else:
+        value = np.Inf
+        for col in valid_moves:
+            child = drop_piece(node, col, mark%2+1, config)
+            value = min(value, minimax_alpha_beta(child, depth-1, alpha, beta, True, mark, config))
+            beta = min(beta, value)
+            if alpha >= beta:
+                break  # Alpha cut-off
+        return value
+    
+N_STEPS = 3
+
+def agent(obs, config):
+    # Get list of valid moves
+    valid_moves = [c for c in range(config.columns) if obs.board[c] == 0]
+    # Convert the board to a 2D grid
+    grid = np.asarray(obs.board).reshape(config.rows, config.columns)
+    # Use the heuristic to assign a score to each possible board in the next step
+    scores = dict(zip(valid_moves, [score_move(grid, col, obs.mark, config, N_STEPS) for col in valid_moves]))
+    # Get a list of columns (moves) that maximize the heuristic
+    max_cols = [key for key in scores.keys() if scores[key] == max(scores.values())]
+    # Select at random from the maximizing columns
+    return random.choice(max_cols)
+
+
+# Create the game environment
+env = make("connectx")
+
+# Two random agents play one game round
+env.run([agent, "random"])
+
+# Show the game
+env.render(mode="ipython")
